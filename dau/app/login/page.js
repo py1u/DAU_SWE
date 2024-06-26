e00ee058f9bef3d6bfc1723579fdda0fe0f3bcaf
kellyserafico <114453810+kellyserafico@users.noreply.github.com>
2024-06-25 22:13:34 -0700

    add html content in login/page.js

--- dau/app/login/page.js
+++ dau/app/login/page.js
@@ -1,5 +1,26 @@
 export default function Login() {
     return(
-        <p>this is the login page</p>
+        <div>
+            <img class="primary-logo" src="../media/images/primary_logo.svg"/>
+                <img class="secondary-logo" src="../media/images/secondary_logo.svg"/>
+                <header class="container-header"><p>Sign In</p></header>
+                <section class="log-in-container">
+                    <div>
+                            <label for="email">email</label>
+                            <input type="text" id="email" placeholder="johndoe@gmail.com"/>
+                    </div>
+                    <div>
+                        <label for="password">password</label>
+                        <input type="password" id="password" placeholder="password"/>
+                    </div>
+                    <span id="divider">
+                        <hr class="solid"/>
+                        OR
+                        <hr class="solid"/>
+                    </span>
+                    <button>create an account</button>
+                </section>
+
+        </div>
     );
 }
