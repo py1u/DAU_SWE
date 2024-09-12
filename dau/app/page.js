70d0364084be485d8c37a7f914a81aa7b3f05497
kellyserafico <seraficok@gmail.com>
2024-09-11 18:18:12 -0700

    add background image

--- dau/app/page.js
+++ dau/app/page.js
@@ -1,20 +1,23 @@
-import Image from "next/image";
+import Image from 'next/image';
+import Logo from "../public/images/primary_logo.svg";
 
 export default function Home() {
   return (
-    <nav className="flex flex-row justify-between h-24">
-        <div className="flex items-center">
-          <a href="#" class="logo">
-            <Image src="../public/images/primary_logo.svg" alt="logo" width="100" height="100" />
-          </a>
-        </div>
-        <div className="flex items-center">
-          <a href="../about/about.html">ABOUT</a>
-          <a href="#">EVENTS</a>
-          <a href="#">RESOURCES</a>
-          <a href="#">CONTACT</a>
-          <a href="#">JOIN</a>
-        </div>
-    </nav>
+    <div>
+      <div className="w-screen h-screen bg-[url('../public/images/bg.png')] bg-cover bg-center">
+      <nav className="flex flex-row justify-between h-24">
+          <div className="flex items-center"> 
+              <Image src={Logo} alt="DAU Logo" className="w-32 h-32"/>
+          </div>
+          <div className="flex items-center space-x-6 text-accent font-bold text-2xl">
+            <a href="../about/about.html">ABOUT</a>
+            <a href="#">EVENTS</a>
+            <a href="#">RESOURCES</a>
+            <a href="#">CONTACT</a>
+            <a href="#">JOIN</a>
+          </div>
+      </nav>
+      </div>
+    </div>
   );
 }
