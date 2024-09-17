dc265094c421bcfd2806e1dca8527ddf85a16d74
kellyserafico <seraficok@gmail.com>
2024-09-17 12:48:20 -0700

    add alumni page

--- dau/app/about/page.js
+++ dau/app/about/page.js
@@ -6,7 +6,7 @@ import GroupPicture from "../../public/images/group_picture.png";
 export default function About(){
     return(
         // <div>
-         <div className="bg-[url('../public/images/about_background.png')] w-screen min-h-screen bg-cover bg-bottom bg-no-repeat flex flex-col justify-between">
+         <div className="bg-[url('../public/images/about_background.png')] w-full min-h-screen bg-cover bg-center bg-no-repeat flex flex-col justify-between">
             <Nav />
                 <h1 className="text-[#D45454] text-7xl font-semibold px-12 py-10">Meet the Board</h1>
                 <section className="flex flex-row flex-wrap justify-center">
@@ -18,6 +18,11 @@ export default function About(){
                     <BoardMember name="Name" position="Position"/>
                     <BoardMember name="Name" position="Position"/>
                     <BoardMember name="Name" position="Position"/>
+                    <BoardMember name="Name" position="Position"/>
+                    <BoardMember name="Name" position="Position"/>
+                    <BoardMember name="Name" position="Position"/>
+                    <BoardMember name="Name" position="Position"/>
+
                 </section>
                 <Image src={GroupPicture} alt="Group Picture" className="m-auto"/>
                 {/* <div className="h-screen"></div>
--- dau/app/components/BoardMember.js
+++ dau/app/components/BoardMember.js
@@ -2,8 +2,8 @@ const BoardMember = ({name, position}) => {
     return(
         <div className="mx-20 my-8">
             <div className="w-64 h-64 bg-white rounded-2xl shadow-button-shadow"></div>
-            <p className="text-[#431D01] text-4xl font-semibold p-1 mt-4">{name}</p>
-            <p className="text-[#431D01] text-4xl p-1">{position}</p>
+            <p className="text-[#431D01] text-4xl font-semibold mt-4">{name}</p>
+            <p className="text-[#431D01] text-4xl">{position}</p>
         </div>
     )
 }
